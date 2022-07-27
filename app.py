d6b3844088e34f91ce67a7a31dc6547d3999e3ec
Stephanie <user@USERs-MacBook-Pro.local>
2022-07-26 20:22:19 -0700

    modified code

--- app.py
+++ app.py
@@ -28,3 +28,26 @@ def scrape():
 
 if __name__ == "__main__":
     app.run()
+
+# visit the mars nasa news site
+url = 'https://redplanetscience.com/'
+browser.visit(url)
+
+# optional delay for loading the page
+browser.its_element_present_by_css('div.list_text', wait_time=1)
+
+#convert the browser html to a soup objecgt and then quit the browser
+html = browser.html
+news_soup = soup(html, 'html.parser')
+
+slide_elem = news_soup.select_one('div.list_text')
+slide_elem.find('div', class_='content_title')
+
+# use the parent element to find the first 'a' tag and save it as 'news_title'
+news_title = slide_elem.find('div', class_='content_title').get_text()
+news_title
+
+# use the parent element to find the paragraph text
+news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+news_p
+
