1d3f2dd3a305cd632673c801d5b5847b035acebc
Stephanie <user@USERs-MacBook-Pro.local>
2022-07-26 19:09:32 -0700

    adding py and ipynb files

---
+++ app.py
@@ -0,0 +1,30 @@
+# import dependecies
+from flask import Flask, render_template, redirect, url_for
+from flask_pymongo import PyMongo
+import scraping
+
+# set up Flask
+app = Flask(__name__)
+
+# use flask_pymongo to set up mongo comnnection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# define HTML route
+@app.route("/")
+def index():
+    mars = mongo.db.mars.find_one()
+    return render_template("index.html", mars = mars)
+
+# set up scraping route
+@app.route("/scrape")
+def scrape():
+    mars = mongo.db.mars
+    mars_data = scraping.scrape_all()
+    mars.update_one({}, {"set":mars_data}, upsert=True)
+    return redirect('/', code = 302)
+
+.update_one(query_parameter, {"$set": data}, options)
+
+if __name__ == "__main__":
+    app.run()
